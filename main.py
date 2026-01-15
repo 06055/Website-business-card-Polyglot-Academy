e41cb3866c5ffdeaebcc94411679968da03a8881
06055 <06055pro@gmail.com>
2026-01-15 20:30:05 +0200

    Initial landing page

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_window():
+    return render_template("polyglot_academy.html")
+
+
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
